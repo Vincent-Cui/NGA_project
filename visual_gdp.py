a0d78ff128863951999ff6b40a72fb5ce64855e6
ccuiww <ccuiww@users.noreply.github.com>
2020-04-01 17:55:27 -0500

    Update visual_gdp.py

--- visual_gdp.py
+++ visual_gdp.py
@@ -46,46 +46,64 @@ df1 = df[filter]
     
 def json_data(selectedYear):
     yr = selectedYear
-    df_yr = df1[df1['yr'] == yr]
-    merged = gdf.merge(df_yr, left_index=True, right_on = 'LGA', how = 'left')
-    merged.fillna({'beta_ndifgdp': 'No data'}, inplace = True)
-    merged_json = json.loads(merged.to_json())
-    json_data = json.dumps(merged_json)
-    return json_data
+    df1_yr = df1[df1['yr'] == yr]
+    df2_yr = df2[df2['yr'] == yr]
+    merged1 = gdf.merge(df1_yr, left_index=True, right_on = 'LGA', how = 'left')
+    merged1.fillna({'beta_ndifgdp': 'No data'}, inplace = True)
+    merged2 = gdf.merge(df2_yr, left_index=True, right_on = 'LGA', how = 'left')
+    merged2.fillna({'beta_ndifpop': 'No data'}, inplace = True)
+    merged_json1 = json.loads(merged1.to_json())
+    json_data1 = json.dumps(merged_json1)
+    merged_json2 = json.loads(merged2.to_json())
+    json_data2 = json.dumps(merged_json2)
+    return json_data1, json_data2
 #Input GeoJSON source that contains features for plotting.
-geosource = GeoJSONDataSource(geojson = json_data(2001))
+json1, json2 = json_data(2001)
+geosource1  = GeoJSONDataSource(geojson = json1)
+geosource2  = GeoJSONDataSource(geojson = json2)
 #Define a sequential multi-hue color palette.
 palette = brewer['YlGnBu'][8]
 #Reverse color order so that dark blue is highest coefficient.
 palette = palette[::-1]
 #Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors. Input nan_color.
-color_mapper = LinearColorMapper(palette = palette, low = 0, high = 10, nan_color = '#d9d9d9')
+color_mapper1 = LinearColorMapper(palette = palette, low = 0, high = 10, nan_color = '#d9d9d9')
+color_mapper2 = LinearColorMapper(palette = palette, low = -0.3, high = 0.5, nan_color = '#d9d9d9')
 #Add hover tool
-hover = HoverTool(tooltips = [ ('LGA ID','@LGA'),('GDP Coef', '@beta_ndifgdp'),('POP Coef', '@beta_ndifpop'), ('Access Coef', '@beta_MEAN_Access'), ('Local R2', '@localR2') ])
+hover = HoverTool(tooltips = [ ('LGA ID','@LGA'), ('Local R2','@localR2'), ('GDP Coef', '@beta_ndifgdp'),('POP Coef', '@beta_ndifpop'), ('Access Coef', '@beta_MEAN_Access') ])
 #Create color bar. 
-color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8,width = 500, height = 20,
+color_bar1 = ColorBar(color_mapper=color_mapper1, label_standoff=8,width = 500, height = 20,
+                     border_line_color=None,location = (0,0), orientation = 'horizontal')
+color_bar2 = ColorBar(color_mapper=color_mapper2, label_standoff=8,width = 500, height = 20,
                      border_line_color=None,location = (0,0), orientation = 'horizontal')
 #Create figure object.
-p = figure(title = 'GDP coef, 2001', plot_height = 600 , plot_width = 600, toolbar_location = None, tools = [hover])
-p.xgrid.grid_line_color = None
-p.ygrid.grid_line_color = None
+p1 = figure(title = 'GDP coef, 2001', plot_height = 600 , plot_width = 600, toolbar_location = None, tools = [hover])
+p2 = figure(title = 'POP coef, 2001', plot_height = 600 , plot_width = 600, toolbar_location = None, tools = [hover])
+p1.xgrid.grid_line_color = None
+p1.ygrid.grid_line_color = None
+p2.xgrid.grid_line_color = None
+p2.ygrid.grid_line_color = None
 #Add patch renderer to figure. 
-p.patches('xs','ys', source = geosource,fill_color = {'field' :'beta_ndifgdp', 'transform' : color_mapper},
+p1.patches('xs','ys', source = geosource1,fill_color = {'field' :'beta_ndifgdp', 'transform' : color_mapper1},
+          line_color = 'black', line_width = 0.25, fill_alpha = 1)
+p2.patches('xs','ys', source = geosource2,fill_color = {'field' :'beta_ndifpop', 'transform' : color_mapper2},
           line_color = 'black', line_width = 0.25, fill_alpha = 1)
 #Specify layout
-p.add_layout(color_bar, 'below')
+p1.add_layout(color_bar1, 'below')
+p2.add_layout(color_bar2, 'below')
 # Define the callback function: update_plot
 def update_plot(attr, old, new):
     yr = slider.value
-    new_data = json_data(yr)
-    geosource.geojson = new_data
-    p.title.text = 'GDP coef, %d' %yr
+    new_data1, new_data2 = json_data(yr)
+    geosource1.geojson = new_data1
+    geosource2.geojson = new_data2
+    p1.title.text = 'GDP coef, %d' %yr
+    p2.title.text = 'POP coef, %d' %yr
     
 # Make a slider object: slider 
 slider = Slider(title = 'Year',start = 2001, end = 2015, step = 1, value = 2001)
 slider.on_change('value', update_plot)
 # Make a column layout of widgetbox(slider) and plot, and add it to the current document
-layout = column(p,widgetbox(slider))
+layout = row(p1,p2,widgetbox(slider))
 curdoc().add_root(layout)
 #Display plot inline in Jupyter notebook
 #output_notebook()
